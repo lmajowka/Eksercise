73f4697ac7ea60ba3b20eb9f296d0a9570434a32
renata <renata@Renatas-MacBook-Pro.local>
2017-10-23 10:27:29 +0300

    small refactor

--- lib/services/eksercise/eksercise_api.rb
+++ lib/services/eksercise/eksercise_api.rb
@@ -2,13 +2,16 @@ module Services
   module Eksercise
     class EkserciseApi
 
-    def self.search(query)
+      API_URL = 'http://eksercise-api.herokuapp.com%s'
+      AUTHENTICATION_HEADERS = { 'X-KLARNA-TOKEN' => 'bc6132fb-16b9-4f6a-ab3c-c5c9017dbcef' }
+
+      def self.search(query)
 
       parsed_query = SearchHelper.new(query).parse_query
 
-      response = HTTParty.post 'http://eksercise-api.herokuapp.com/people/search',
+      response = HTTParty.post API_URL % '/people/search',
                                {
-                                   headers: authentication_headers,
+                                   headers: AUTHENTICATION_HEADERS,
                                    body: parsed_query
                                }
 
@@ -16,23 +19,15 @@ module Services
       search_id = parsed_response['id']
 
       sleep(25)
-      
-      response2 = HTTParty.get "http://eksercise-api.herokuapp.com/people?searchRequestId=#{search_id}",
+
+      response2 = HTTParty.get API_URL % "/people?searchRequestId=#{search_id}",
                                {
-                                headers: authentication_headers
+                                headers: AUTHENTICATION_HEADERS
                                }
 
 
       parsed_response2 = JSON.parse response2.body
       parsed_response2
-    end
-
-    private
-
-    def self.authentication_headers
-      {
-          'X-KLARNA-TOKEN' => 'bc6132fb-16b9-4f6a-ab3c-c5c9017dbcef'
-      }
     end
 
     end
